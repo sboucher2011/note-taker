8c3f3d1bf606e523733eaa69ced00cf12b20b0a5
sboucher <sboucher2011@gmail.com>
2022-01-07 20:01:49 -0500

    post route created

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,23 @@
 const path = require("path");
 const router = require("express").Router();
+const { findById, createNewNote, validateNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
+    
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    }
 });
   
 module.exports = router;
