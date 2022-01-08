93e6a5582eec694223f520ebf9a36e264124c0ad
sboucher <sboucher2011@gmail.com>
2022-01-07 19:07:10 -0500

    added api connection

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const router = require("express").Router();
+const { notes } = require('../../db/db');
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+  
+module.exports = router;
